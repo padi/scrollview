c1203b555dc2df67e875b74144ea0d57988525a8
Marc Rendl Ignacio <boracay+marc@aelogica.com>
2012-10-04 13:53:48 +0800

    Gemfile and HomeViewController

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source :rubygems
+
+gem 'rubymotion_generators'
+gem 'sugarcube'
+gem 'geomotion'
--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,5 +1,10 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
+    @window = UIWindow.alloc.initWithFrame UIScreen.mainScreen.bounds
+    @window.makeKeyAndVisible
+    home_controller = HomeViewController.alloc.init
+    @window.rootViewController = home_controller
+
     true
   end
 end
